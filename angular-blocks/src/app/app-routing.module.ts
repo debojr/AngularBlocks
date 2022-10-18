db545dd1f074e113248305ac19a77cbf071d409e
debojr <debeljak.jan@gmail.com>
2022-10-18 21:08:50 +0200

    added routing

--- angular-blocks/src/app/app-routing.module.ts
+++ angular-blocks/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { ParallaxComponent } from './parallax/parallax.component';
+import { FirstComponent } from './first/first.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { first } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'first', component: FirstComponent},
+  { path: 'parallax', component: ParallaxComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FirstComponent, ParallaxComponent]
